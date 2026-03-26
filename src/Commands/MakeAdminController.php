07fa0ecad741fcda45f0e2ed7e4f0cebdedab222
人在旅途 <lvtu@dabashan.cc>
2026-03-27 03:23:14 +0800

    1

--- src/Commands/MakeAdminController.php
+++ src/Commands/MakeAdminController.php
@@ -7,46 +7,70 @@ use Illuminate\Support\Str;
 
 class MakeAdminController extends Command
 {
-    protected $signature = 'make:admin-controller {name : Controller name (e.g. User, Order)}';
+    protected $signature = 'make:admin-controller
+                            {name : Controller name (e.g. User, Order)}
+                            {--view-name= : Custom Vue view directory name (kebab-case)}';
 
-    protected $description = 'Create a new Admin Core controller with Grid/Form, Vue page and API file';
+    protected $description = 'Create a new Admin Core controller with Grid/Form, Vue page, API, Router and Locale files';
 
     public function handle(): int
     {
         $name = $this->argument('name');
         $studlyName = Str::studly($name);
         $kebabName = Str::kebab($name);
+        $viewName = $this->option('view-name') ?: $kebabName;
         $modelName = 'Admin' . $studlyName;
         $controllerName = $studlyName . 'Controller';
+        $pluralKebab = Str::plural($kebabName);
+        $titleEn = $studlyName . ' Management';
 
-        // 1. Generate Controller
-        $controllerPath = app_path("Admin/Controllers/Core/{$controllerName}.php");
-        $this->generateFile($controllerPath, 'controller.core.stub', [
+        $replacements = [
             '{{ class }}' => $controllerName,
             '{{ model }}' => $modelName,
             '{{ name }}' => $studlyName,
-        ]);
+            '{{ kebabName }}' => $kebabName,
+            '{{ viewName }}' => $viewName,
+            '{{ parentPath }}' => 'system',
+            '{{ apiPrefix }}' => '/admin/' . $pluralKebab,
+            '{{ title }}' => $studlyName,
+            '{{ titleEn }}' => $titleEn,
+            '{{ pluralKebab }}' => $pluralKebab,
+        ];
+
+        // 1. Generate Controller
+        $controllerPath = app_path("Admin/Controllers/Core/{$controllerName}.php");
+        $this->generateFile($controllerPath, 'controller.core.stub', $replacements);
 
         // 2. Generate Vue page
-        $vuePath = base_path("web/src/views/system/{$kebabName}/index.vue");
-        $this->generateFile($vuePath, 'vue-page.stub', [
-            '{{ name }}' => $studlyName,
-            '{{ kebabName }}' => $kebabName,
-            '{{ title }}' => $studlyName . ' Management',
-        ]);
+        $vuePath = base_path("web/src/views/system/{$viewName}/index.vue");
+        $this->generateFile($vuePath, 'vue-page.stub', $replacements);
 
         // 3. Generate API file
         $apiPath = base_path("web/src/api/{$kebabName}.ts");
-        $this->generateFile($apiPath, 'vue-api.stub', [
-            '{{ name }}' => $studlyName,
-            '{{ kebabName }}' => $kebabName,
-            '{{ apiPrefix }}' => '/admin/' . Str::plural($kebabName),
-        ]);
+        $this->generateFile($apiPath, 'vue-api.stub', $replacements);
+
+        // 4. Generate Router module file
+        $routerPath = base_path("web/src/router/routes/modules/system-{$kebabName}.ts");
+        $this->generateFile($routerPath, 'vue-router-core.stub', $replacements);
+
+        // 5. Generate Locale files
+        $localeZhPath = base_path("web/src/views/system/{$viewName}/locale/zh-CN.ts");
+        $this->generateFile($localeZhPath, 'vue-locale-zh.stub', $replacements);
+
+        $localeEnPath = base_path("web/src/views/system/{$viewName}/locale/en-US.ts");
+        $this->generateFile($localeEnPath, 'vue-locale-en.stub', $replacements);
 
         $this->info('Admin Controller created successfully:');
         $this->line("  Controller: {$controllerPath}");
         $this->line("  Vue Page:   {$vuePath}");
         $this->line("  API File:   {$apiPath}");
+        $this->line("  Router:     {$routerPath}");
+        $this->line("  Locale ZH:  {$localeZhPath}");
+        $this->line("  Locale EN:  {$localeEnPath}");
+        $this->newLine();
+        $this->warn('Note: You may need to import the locale file in your main locale config:');
+        $this->line("  import locale{$studlyName} from '@/views/system/{$viewName}/locale/zh-CN';");
+        $this->line("  Then spread it: ...locale{$studlyName}");
 
         return Command::SUCCESS;
     }
--- src/Commands/MakeAdminPluginController.php
+++ src/Commands/MakeAdminPluginController.php
@@ -7,11 +7,12 @@ use Illuminate\Support\Str;
 
 class MakeAdminPluginController extends Command
 {
-    protected $signature = 'make:admin-plugin-controller 
-                            {plugin : Plugin name (e.g. DemoPlugin)}
-                            {name : Controller name (e.g. User, Order)}';
+    protected $signature = 'make:admin-plugin-controller
+                            {plugin : Plugin name (e.g. DemoPlugin, Shop)}
+                            {name : Controller name (e.g. User, Order)}
+                            {--view-name= : Custom Vue view directory name (kebab-case)}';
 
-    protected $description = 'Create a new Admin Plugin controller with Grid/Form, Vue page and API file';
+    protected $description = 'Create a new Admin Plugin controller with Grid/Form, Vue page, API, Router and Locale files';
 
     public function handle(): int
     {
@@ -22,38 +23,62 @@ class MakeAdminPluginController extends Command
         $pluginKebab = Str::kebab($plugin);
         $studlyName = Str::studly($name);
         $kebabName = Str::kebab($name);
+        $viewName = $this->option('view-name') ?: $kebabName;
         $modelName = $studlyName;
         $controllerName = $studlyName . 'Controller';
+        $pluralKebab = Str::plural($kebabName);
+        $titleEn = $studlyName . ' Management';
 
-        // 1. Generate Controller
-        $controllerPath = app_path("Admin/Controllers/Plugins/{$pluginStudly}/{$controllerName}.php");
-        $this->generateFile($controllerPath, 'controller.plugin.stub', [
+        $replacements = [
             '{{ class }}' => $controllerName,
             '{{ model }}' => $modelName,
             '{{ name }}' => $studlyName,
             '{{ plugin }}' => $pluginStudly,
-        ]);
+            '{{ pluginName }}' => $pluginStudly,
+            '{{ pluginKebab }}' => $pluginKebab,
+            '{{ kebabName }}' => $kebabName,
+            '{{ viewName }}' => $viewName,
+            '{{ parentPath }}' => "plugin.{$pluginKebab}",
+            '{{ apiPrefix }}' => '/admin/plugins/' . $pluginKebab . '/' . $pluralKebab,
+            '{{ title }}' => $studlyName,
+            '{{ titleEn }}' => $titleEn,
+            '{{ pluralKebab }}' => $pluralKebab,
+        ];
+
+        // 1. Generate Controller
+        $controllerPath = app_path("Admin/Controllers/Plugins/{$pluginStudly}/{$controllerName}.php");
+        $this->generateFile($controllerPath, 'controller.plugin.stub', $replacements);
 
         // 2. Generate Vue page
-        $vuePath = base_path("web/src/views/plugin/{$pluginKebab}/{$kebabName}/index.vue");
-        $this->generateFile($vuePath, 'vue-page.stub', [
-            '{{ name }}' => $studlyName,
-            '{{ kebabName }}' => $kebabName,
-            '{{ title }}' => $studlyName . ' Management',
-        ]);
+        $vuePath = base_path("web/src/views/plugin/{$pluginKebab}/{$viewName}/index.vue");
+        $this->generateFile($vuePath, 'vue-page.stub', $replacements);
 
         // 3. Generate API file
         $apiPath = base_path("web/src/api/{$pluginKebab}-{$kebabName}.ts");
-        $this->generateFile($apiPath, 'vue-api.stub', [
-            '{{ name }}' => $studlyName,
-            '{{ kebabName }}' => $kebabName,
-            '{{ apiPrefix }}' => '/admin/plugins/' . $pluginKebab . '/' . Str::plural($kebabName),
-        ]);
+        $this->generateFile($apiPath, 'vue-api.stub', $replacements);
+
+        // 4. Generate Router module file
+        $routerPath = base_path("web/src/router/routes/modules/plugin-{$pluginKebab}-{$kebabName}.ts");
+        $this->generateFile($routerPath, 'vue-router-plugin.stub', $replacements);
+
+        // 5. Generate Locale files
+        $localeZhPath = base_path("web/src/views/plugin/{$pluginKebab}/{$viewName}/locale/zh-CN.ts");
+        $this->generateFile($localeZhPath, 'vue-locale-zh.stub', $replacements);
+
+        $localeEnPath = base_path("web/src/views/plugin/{$pluginKebab}/{$viewName}/locale/en-US.ts");
+        $this->generateFile($localeEnPath, 'vue-locale-en.stub', $replacements);
 
         $this->info('Admin Plugin Controller created successfully:');
         $this->line("  Controller: {$controllerPath}");
         $this->line("  Vue Page:   {$vuePath}");
         $this->line("  API File:   {$apiPath}");
+        $this->line("  Router:     {$routerPath}");
+        $this->line("  Locale ZH:  {$localeZhPath}");
+        $this->line("  Locale EN:  {$localeEnPath}");
+        $this->newLine();
+        $this->warn('Note: You may need to import the locale file in your main locale config:');
+        $this->line("  import locale{$pluginStudly}{$studlyName} from '@/views/plugin/{$pluginKebab}/{$viewName}/locale/zh-CN';");
+        $this->line("  Then spread it: ...locale{$pluginStudly}{$studlyName}");
 
         return Command::SUCCESS;
     }
